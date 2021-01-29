f563ba4d10a860691f83ad190e43aea310196b46
KacperKenjiLesniak <kenjikacper@gmail.com>
2021-01-29 19:27:33 +0100

    Boat collision detection

--- Assets/Scripts/Boat/BoatCollision.cs
+++ Assets/Scripts/Boat/BoatCollision.cs
@@ -1,9 +1,31 @@
-﻿using UnityEngine;
+﻿using System;
+using MutableObjects.Int;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Boat
 {
     public class BoatCollision : MonoBehaviour
     {
-        
+        [SerializeField] private MutableInt boatHealth;
+        [SerializeField] private float collisionSpeedThreshold;
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.collider.CompareTag("Obstacle"))
+            {
+                Debug.Log("Collided with an obstacle");
+                Debug.Log("Collision speed: " + other.relativeVelocity.magnitude);
+                if (other.relativeVelocity.magnitude > collisionSpeedThreshold)
+                {
+                    boatHealth.Value -= 1;
+                    Debug.Log("Boat health: " + boatHealth.Value);
+                    if (boatHealth.Value <= 0)
+                    {
+                        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                    }
+                }
+            }
+        }
     }
 }
