106c861e3e5d51b93839025ce5de1f6eee5e85bb
KacperKenjiLesniak <kenjikacper@gmail.com>
2021-01-29 19:35:32 +0100

    Small lighthouse fixes

--- Assets/Scripts/Lighthouse/RotateLight.cs
+++ Assets/Scripts/Lighthouse/RotateLight.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class RotateLight : MonoBehaviour
 {
-
     public float rotationSpeed;
 
-
-    // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        transform.Rotate(0f, 0f, rotationSpeed);
-        if (transform.rotation.z==360)
+        transform.Rotate(0f, 0f, rotationSpeed * Time.deltaTime);
+        if (transform.rotation.z >= 360f)
         {
             transform.rotation = Quaternion.Euler(0f, 0f, 0f);
         }
