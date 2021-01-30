2040358ec5fbe34a856e3240e61c8144fd75f141
KacperKenjiLesniak <kenjikacper@gmail.com>
2021-01-30 14:47:33 +0100

    Oil lamp fixes

--- Assets/Scripts/Boat/OilLantern.cs
+++ Assets/Scripts/Boat/OilLantern.cs
@@ -3,19 +3,27 @@ using System.Collections.Generic;
 using MutableObjects.Bool;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering.Universal;
+using UnityEngine.UIElements;
 
 public class OilLantern : MonoBehaviour
 {
     [SerializeField] private float maxOilSeconds = 20f;
     [SerializeField] private float oilSeconds = 20f;
     [SerializeField] private float fullLanternRadius;
+    [SerializeField] private float innerLightRadius;
     [SerializeField] private float dimmingLightThreshold = 10f;
     [SerializeField] private float refillSpeed = 2.6f;
     [SerializeField] private MutableBool movementActive;
 
+    private Light2D light;
+    private float ratio;
+    
     void Start()
     {
-        fullLanternRadius = GetComponent<Light2D>().pointLightOuterRadius;
+        light = GetComponent<Light2D>();
+        fullLanternRadius = light.pointLightOuterRadius;
+        innerLightRadius = light.pointLightInnerRadius;
+        ratio = innerLightRadius / fullLanternRadius;
     }
 
     void Update()
@@ -27,7 +35,7 @@ public class OilLantern : MonoBehaviour
 
         if (oilSeconds < dimmingLightThreshold)
         {
-            GetComponent<Light2D>().pointLightOuterRadius = fullLanternRadius * (oilSeconds / dimmingLightThreshold);
+            light.pointLightOuterRadius = fullLanternRadius * (oilSeconds / dimmingLightThreshold);
             if (Input.GetKey(KeyCode.Space))
             {
                 movementActive.Value = false;
@@ -35,9 +43,11 @@ public class OilLantern : MonoBehaviour
                 if (oilSeconds >= dimmingLightThreshold)
                 {
                     oilSeconds = maxOilSeconds;
-                    GetComponent<Light2D>().pointLightOuterRadius = fullLanternRadius;
+                    light.pointLightOuterRadius = fullLanternRadius;
                 }
             }
+
+            light.pointLightInnerRadius = ratio * light.pointLightOuterRadius;
         }
 
         if (Input.GetKeyUp(KeyCode.Space))
