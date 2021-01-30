f4b2d1be8696dc836471d3544351300014d172f3
KacperKenjiLesniak <kenjikacper@gmail.com>
2021-01-30 23:59:54 +0100

    Ping sound

---
+++ Assets/Scripts/Goal/SignalAnimation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEditor.Timeline.Actions;
+using UnityEngine;
+
+namespace UI
+{
+    public class SignalAnimation : MonoBehaviour
+    {
+        private void Start()
+        {
+            InvokeRepeating(nameof(PlaySignalAnimation), 0f, 8f);
+        }
+
+        private void PlaySignalAnimation()
+        {
+            GetComponent<Animator>().SetTrigger("Fade");
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
