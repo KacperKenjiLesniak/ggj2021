880d4d4776c03fd6566e652980b068455a1a81ce
KacperKenjiLesniak <kenjikacper@gmail.com>
2021-01-31 10:05:39 +0100

    Longer signal animation

--- Assets/Scripts/Goal/SignalAnimation.cs
+++ Assets/Scripts/Goal/SignalAnimation.cs
@@ -4,11 +4,27 @@ namespace UI
 {
     public class SignalAnimation : MonoBehaviour
     {
+        [SerializeField] private float firstPing = 2f;
+        [SerializeField] private float secondPing = 8f;
+        [SerializeField] private float nextPings = 20f;
+        
         private void Start()
         {
-            InvokeRepeating(nameof(PlaySignalAnimation), 2f, 8f);
+            Invoke(nameof(PlayFirstAnimation), firstPing);
         }
 
+        private void PlayFirstAnimation()
+        {
+            GetComponent<Animator>().SetTrigger("Fade");
+            GetComponent<AudioSource>().Play();
+            Invoke(nameof(PlaySecondAnimation), secondPing);
+        }
+        
+        private void PlaySecondAnimation()
+        {
+            InvokeRepeating(nameof(PlaySignalAnimation), 0f, nextPings);
+        }
+        
         private void PlaySignalAnimation()
         {
             GetComponent<Animator>().SetTrigger("Fade");
