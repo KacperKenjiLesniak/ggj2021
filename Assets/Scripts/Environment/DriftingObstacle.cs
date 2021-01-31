ebe976a1e90ba237297342bcaa6f75211ddab8de
KacperKenjiLesniak <kenjikacper@gmail.com>
2021-01-31 10:22:00 +0100

    Drifting wood obstacles activation

--- Assets/Scripts/Environment/DriftingObstacle.cs
+++ Assets/Scripts/Environment/DriftingObstacle.cs
@@ -1,15 +1,24 @@
-﻿using UnityEngine;
+﻿using MutableObjects.Vector3;
+using UnityEngine;
 using Random = UnityEngine.Random;
 
 namespace Obstacles
 {
     public class DriftingObstacle : MonoBehaviour
     {
+        [SerializeField] private MutableVector3 boatPosition;
+        [SerializeField] private float distanceToActivate = 20f;
         [SerializeField] private float randomForceStrength;
-        
-        private void Start()
+
+        private bool activated;
+
+        private void Update()
         {
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(Random.Range(-randomForceStrength, randomForceStrength), Random.Range(-randomForceStrength, randomForceStrength)));
+            if (!activated && Vector2.Distance(boatPosition.Value, transform.position) < distanceToActivate)
+            {
+                GetComponent<Rigidbody2D>().AddForce(new Vector2(Random.Range(-randomForceStrength, randomForceStrength), Random.Range(-randomForceStrength, randomForceStrength)));
+                activated = true;
+            }
         }
     }
 }
