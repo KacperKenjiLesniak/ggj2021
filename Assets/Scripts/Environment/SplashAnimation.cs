8f6f2f515e2502c11c930c1e485c0202f9540389
KacperKenjiLesniak <kenjikacper@gmail.com>
2021-01-31 10:33:53 +0100

    More splashes and faster planks

---
+++ Assets/Scripts/Environment/SplashAnimation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Obstacles
+{
+    public class SplashAnimation : MonoBehaviour
+    {
+        private void Start()
+        {
+            var anim = GetComponent<Animator>();
+            var randomIdleStart = Random.Range(0,anim.GetCurrentAnimatorStateInfo(0).length);
+            anim.Play("Splash1", 0, randomIdleStart);
+        }
+    }
+}
